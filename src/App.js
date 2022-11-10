14fbe1bf97de550b1632105ee02d2d4554ae744b
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-10 17:40:45 -0300

    Instalado y configurado Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
+import firebase, { FirebaseContext } from './firebase'
+
 import Ordenes from './components/views/Ordenes'
 import Menu from './components/views/Menu'
 import NuevoPlatillo from './components/views/NuevoPlatillo'
@@ -8,18 +10,20 @@ import Sidebar from './components/ui/Sidebar'
 
 function App() {
   return (
-    <div className='md:flex min-h-screen'>
-      <Sidebar/>
+    <FirebaseContext.Provider value={{ firebase }}>
+      <div className='md:flex min-h-screen'>
+        <Sidebar/>
 
-      <div className='md:w-3/5 xl:w-4/5 p-6'>
-        <Routes>
-          <Route path="/" element={ <Ordenes /> } />
-          <Route path="/menu" element={ <Menu /> } />
-          <Route path="/nuevo-platillo" element={ <NuevoPlatillo /> } />
-          <Route path="*" element={ <Navigate to='/' /> } />
-        </Routes>
+        <div className='md:w-3/5 xl:w-4/5 p-6'>
+          <Routes>
+            <Route path="/" element={ <Ordenes /> } />
+            <Route path="/menu" element={ <Menu /> } />
+            <Route path="/nuevo-platillo" element={ <NuevoPlatillo /> } />
+            <Route path="*" element={ <Navigate to='/' /> } />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </FirebaseContext.Provider>
   );
 }
 
