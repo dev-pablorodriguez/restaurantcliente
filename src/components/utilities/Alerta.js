96676956764420d7b55e336c2ee614d99a0a1ffa
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-10 23:00:36 -0300

    Fix Alerta

--- src/components/utilities/Alerta.js
+++ src/components/utilities/Alerta.js
@@ -1,6 +1,6 @@
-const Alerta = ({ title = 'Error', message = 'Se ha producido un error desconocido.', color = 'red' }) => {
+const Alerta = ({ title = 'Error', message = 'Se ha producido un error desconocido.' }) => {
     return (
-        <div className={ `border-l-4 bg-red-100 border-${ color }-500 text-${ color }-800 p-2 mx-5 my-2` } role='alert'>
+        <div className={ `border-l-4 bg-red-100 border-red-500 text-red-800 p-2 mx-5 my-2` } role='alert'>
             <p className='text-sm'>
                 <span className='font-bold'>
                  { `${ title }: ` }
